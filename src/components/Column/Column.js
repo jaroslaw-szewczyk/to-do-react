48bf2e736507c4a443321b6aa73c5a0de40fa9f7
Jarosław Szewczyk <jaroslaw.szewczyk@tutamail.com>
2024-11-09 14:57:07 +0000

    Add react-redux with useSelector in components

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,15 +1,21 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
 const Column = props => {
+
+  const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
+
+  console.log(props);
+
   return(
     <article className={styles.column}>
       <span className={styles.icon + ' fa fa-' + props.icon}></span><h2 className={styles.title}>{props.title}</h2>
       <ul className={styles.cards}>
-        {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+        {cards.map(card => <Card key={card.id} title={card.title} />)}
       </ul>
-      <CardForm columnId={props.id} action={props.action}/>
+      <CardForm columnId={cards.id} action={props.action}/>
     </article>
   );
 };
