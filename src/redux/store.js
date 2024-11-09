b783c5f3cc178421f7cbd1e11d4bba8421b27d22
Jarosław Szewczyk <jaroslaw.szewczyk@tutamail.com>
2024-11-09 14:26:36 +0000

    Add initialStore.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,13 @@
 import { createStore } from 'redux';
+import initialStore from './initialStore';
 
 const reducer = (state, action) => {
   return state;
 };
 
-const initialState = {
-  columns: []
-};
-
 const store = createStore(
   reducer,
-  initialState,
+  initialStore,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
