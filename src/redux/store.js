d639e90e03ae4a00c036c66346881a113e2b8c91
Jarosław Szewczyk <jaroslaw.szewczyk@tutamail.com>
2024-11-17 21:50:49 +0000

    Modyfiy List component, add getListById getColumnsByList to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,8 @@ import { strContains } from '../utils/strContains';
 
 export const getFilteredCards = ({cards, searchPhrase}, columnId) => cards.filter(card => card.columnId === columnId && strContains(card.title, searchPhrase));
 export const getAllColumns = (({ columns }) => columns);
+export const getListById = ({ lists },id) => lists.find(iteam => iteam.id === id);
+export const getColumnsByList = ({ columns }, listId) => columns.filter(column => column.listId === listId)
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 export const addCard = payload => ({type: 'ADD_CARD', payload});
 export const updateSearchiString = payload => ({type: 'SEARCH', payload});
